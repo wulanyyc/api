4bac5a42d6635f5427417a82ac1855aabea175db
yangyuncai <yyc@yunlaba.com>
2018-04-11 10:48:22 +0800

    test product add

--- batch.sql
+++ batch.sql
@@ -4,7 +4,6 @@ CHANGE COLUMN `img` `img` VARCHAR(255) NULL DEFAULT '' ;
 ALTER TABLE `biaoye`.`product_list` 
 DROP COLUMN `deleteflag`;
 
-
 ALTER TABLE `biaoye`.`agent` 
 CHANGE COLUMN `status` `status` TINYINT(1) NULL DEFAULT '0' COMMENT '0: 审核中  1: 有效  2: 审核失败  3: 禁用 ' ;
 
