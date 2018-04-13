d689ea2e077b626684c4bb2a1065dc4d9b205887
yangyuncai <yyc@yunlaba.com>
2018-04-13 10:52:10 +0800

    add rob job

--- app/controllers/v1/app/agent.php
+++ app/controllers/v1/app/agent.php
@@ -39,52 +39,48 @@ $app->get('/v1/app/agent/job', function () use ($app) {
 });
 
 
-$app->get('/v1/app/agent/get/job/{oid:\d+}', function ($oid) use ($app) {
+$app->get('/v1/app/agent/rob/job/{oid:\d+}', function ($oid) use ($app) {
     $id = $app->util->getAgentId($app);
 
     $key = $app->config->params->get_order_prefix . $oid;
 
-    if (!$app->redis->exsits($key)) {
+    if (!$app->redis->exists($key)) {
         throw new BusinessException(1000, '该单已过期');
     }
 
-    $getNum = $app->redis->get($key);
-    if ($getNum == 1) {
-        $app->redis->incr($key);
-        $exsit = AgentOrderSuc::count("order_id=" . $oid);
-        if ($exsit > 0) {
-            $list = new AgentOrderList();
-            $list->agent_id = $id;
-            $list->order_id = $oid;
-            $list->save();
-            throw new BusinessException(1000, '该单已被抢');
-        }
+    $num = $app->redis->get($key);
+    if ($num < 1) {
+        // 抢单redis事务
+        $app->redis->watch($key);
+        $app->redis->multi();
+        $app->redis->set($key, $num + 1);
+        $robStatus = $app->redis->exec();
 
-        $ar = new AgentOrderSuc();
-        $ar->agent_id = $id;
-        $ar->order_id = $oid;
+        if ($robStatus) {
+            $ar = new AgentOrderSuc();
+            $ar->agent_id = $id;
+            $ar->order_id = $oid;
 
-        if ($ar->save()) {
-            $list = new AgentOrderList();
-            $list->agent_id = $id;
-            $list->order_id = $oid;
-            $list->status = 1;
-            $list->save();
+            if ($ar->save()) {
+                AgentOrderList::addRecord($id, $oid, 1);
+                $co = CustomerOrder::findFirst($oid);
+                $co->status = 2;
+                $co->save();
 
-            return $ar->id;
+                return $ar->id;
+            } else {
+                AgentOrderList::addRecord($id, $oid);
+
+                throw new BusinessException(1000, '该单已被抢');
+            }
         } else {
-            $list = new AgentOrderList();
-            $list->agent_id = $id;
-            $list->order_id = $oid;
-            $list->save();
-            throw new BusinessException(1000, '该单已被抢');
+            AgentOrderList::addRecord($id, $oid);
+
+            throw new BusinessException(1000, '抢单失败');
         }
     } else {
-        $app->redis->incr($key);
-        $list = new AgentOrderList();
-        $list->agent_id = $id;
-        $list->order_id = $oid;
-        $list->save();
+        AgentOrderList::addRecord($id, $oid);
+
         throw new BusinessException(1000, '该单已被抢');
     }
 });
--- app/models/AgentOrderList.php
+++ app/models/AgentOrderList.php
@@ -8,4 +8,12 @@ class AgentOrderList extends Model
     public function getSource() {
         return "agent_order_list";
     }
+
+    public static function addRecord($agentId, $orderId, $status = 0) {
+        $ar = new self();
+        $ar->agent_id = $agentId;
+        $ar->order_id = $orderId;
+        $ar->status = $status;
+        $ar->save();
+    }
 }
