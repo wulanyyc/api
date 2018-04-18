2a63951d7bff14ab53185ebbd3ac0d4de77bc98a
yangyuncai <yyc@yunlaba.com>
2018-04-18 10:42:16 +0800

    test cart

--- app/controllers/v1/h5/cart.php
+++ app/controllers/v1/h5/cart.php
@@ -109,10 +109,12 @@ $app->post('/v1/h5/cart/update/{cid:\d+}', function ($cid) use ($app) {
     }
 
     $cart = [];
-    $app->logger->error($updateCart);
-    $app->logger->error(json_encode($cartInfo));
+    // $app->logger->error($updateCart);
+    // $app->logger->error(json_encode($cartInfo));
     foreach($cartInfo as $item) {
-        if (is_int($item['id']) && is_int($item['num']) && $item['id'] > 0 && $item['num'] > 0) {
+        $item['id'] = intval($item['id']);
+        $item['num'] = intval($item['num']);
+        if ($item['id'] > 0 && $item['num'] > 0) {
             $cart[$item['id']] = [
                 'id' => $item['id'],
                 'num' => $item['num'],
